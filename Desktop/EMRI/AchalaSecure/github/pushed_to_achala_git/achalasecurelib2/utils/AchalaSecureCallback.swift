5c3a5c2f2f242473cb1ff1827c38a4e0c387593e
Moses Burla <moses.burla@achalasolutions.com>
2025-04-04 17:59:22 +0530

    Removed

--- Desktop/EMRI/AchalaSecure/github/pushed_to_achala_git/achalasecurelib2/utils/AchalaSecureCallback.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import UIKit
-protocol AchalaSecureCallback: AnyObject {
-    func onCompareSuccess(result: String, score: String,capturedImage:UIImage)
-    func onCompareFailed(failed: String,score: String)
-}
